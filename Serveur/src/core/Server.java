94f078076cd42c5f10594a5daaa0cea6c4d24f7f
Adriean Khisbe <adriean.khisbe@live.fr>
2013-11-18 22:54:43 +0100

    Server: improved handler: timeout to ensure vivacity

--- Serveur/src/core/Server.java
+++ Serveur/src/core/Server.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -27,16 +28,14 @@ public class Server implements Runnable {
 	// Threads and connexions
 	protected LinkedList<Socket> waitingSockets;
 	private ConnexionStacker cs;
-	private ConnexionHandler ch;
+	private ConnexionHandler ch[]; 
 	// TODO Thread handler
 	// private GameManager gm;
-	
 
 	// EXT Spectateur
 	protected ArrayList<Joueur> joueurs;
 
 	protected ArrayList<Partie> parties; // TODO ? distinguer partie, et round?
-
 
 	/**
 	 * Simple constructeurs
@@ -52,7 +51,8 @@ public class Server implements Runnable {
 
 			waitingSockets = new LinkedList<Socket>();
 			cs = new ConnexionStacker();
-			ch = new ConnexionHandler();
+			ch = new ConnexionHandler[2]; //PARAM
+			for(int i = 0;i<ch.length;i++) ch[i] = new ConnexionHandler();
 
 			dico = new Dictionnaire(opt.dico);
 			joueurs = new ArrayList<Joueur>();
@@ -62,7 +62,7 @@ public class Server implements Runnable {
 			// TODO: initialisation des threads
 
 		} catch (IOException e) {
-			// TODO To improve error handling
+			// TODO To improve error handling (ressayer ouvrir socket
 			System.err.println("error:" + e.getMessage());
 			// e.printStackTrace();
 			System.exit(1);
@@ -101,15 +101,14 @@ public class Server implements Runnable {
 	 */
 
 	public void run() {
-		// TODO 
+		// TODO
 
 		cs.start();
-		ch.start();
-		
+		for(ConnexionHandler i : ch) i.start();
+		//ch.start();
 
 	}
 
-	
 	/**
 	 * Stack new connexion
 	 */
@@ -131,7 +130,7 @@ public class Server implements Runnable {
 			}
 
 		}
-	}  // end of ConnexionStacker
+	} // end of ConnexionStacker
 
 	/*******
 	 * Handle Connection
@@ -140,74 +139,101 @@ public class Server implements Runnable {
 
 	class ConnexionHandler extends Thread {
 
-		
-		//TODO: changer: blockant sur la socket traitée en cours!!
-		//REFACTOR!!!
+		// TODO: changer: blockant sur la socket traitée en cours!!
+		// REFACTOR!!!
 		public void run() {
 			Socket client;
 			BufferedReader inchan;
 			DataOutputStream outchan;
 
 			// Q? ou variables instances
-
+			
+			HandleLoop: 
 			while (true) {
 				synchronized (waitingSockets) {
-					if (!waitingConnexion()) {
+					while (!waitingConnexion()) {
 						try {
+							IO.traceDebug("Attente Connexion Socket");
 							waitingSockets.wait();
-							// at
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-					} else {
-						IO.trace("Traitement connexion en attente.");
-						client = takeWaitingSocket();
-						// BONUX handle error.
+					}
 
-						try {
+					IO.trace("Traitement connexion en attente.");
+					client = takeWaitingSocket();
+				}
+					// BONUX handle error.
 
-							inchan = new BufferedReader(new InputStreamReader(
-									client.getInputStream()));
-							outchan = new DataOutputStream(
-									client.getOutputStream());
+					try {
 
-							// Traitement selon commande reçue
+						inchan = new BufferedReader(new InputStreamReader(
+								client.getInputStream()));
+						outchan = new DataOutputStream(client.getOutputStream());
 
-							String command = inchan.readLine();
+						// Met un timeout à la lecture sur la socket
+						client.setSoTimeout(3000); //BONUX: temps augmente au fur et à mesure
 
-							if (command.equals("CONNECT/")) {
-								// parser
-								outchan.writeChars("CONNECTED/\n");
+						// Traitement selon commande reçue
+						String command = null;
 
-								// TODO : GET HERE
-
-							} else {
-
-								outchan.writeChars("GOODBYE/BOLOS/\n");
-								inchan.close();
-								outchan.close();
-								client.close();
-
+						// WARNING: is that the good way to do?
+						TryReadTimeout: while(true){
+							try {
+								command = inchan.readLine();
+								
+								// Handling of ActionsScript ask
+								// if(command.equals() )
+								
+								break TryReadTimeout; 							
+								
+							} catch (SocketTimeoutException ste) {
+								//
+								if (waitingConnexion()) {
+									IO.traceDebug("Renvoie dormir");
+								//	inchan.close();outchan.close();
+									synchronized (waitingSockets) {
+										waitingSockets.add(client);
+										waitingSockets.notify();
+									}
+									continue HandleLoop;
+								} else { // si personne attend
+									IO.traceDebug("Et un tour de manège");
+									continue TryReadTimeout;
+								}
 							}
+						}
+						// NOTA: was intialy a dowhile(false), mais marche pas bien sur avec continue
+						
+						
 
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						if (command.equals("CONNECT/")) {
+							// parser
+							outchan.writeChars("CONNECTED/\n");
+
+							// TODO : GET HERE
+
+						} else {
+
+							outchan.writeChars("GOODBYE/BOLOS/\n");
+							IO.traceDebug("Kick out Bolos");
+							inchan.close();
+							outchan.close();
+							client.close();
 
 						}
+
+					} catch (IOException e) {
+						// TODO ?? rajoute
+						e.printStackTrace();
 
 					}
 
 				}
 
-			}
 
 		}
-	}  // end of ConnexionHandler
+	} // end of ConnexionHandler
 
-	
-	
-	
-	
 }
